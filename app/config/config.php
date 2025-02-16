edf9979c075a8a382889bf4f71047cbfb23c181b
Erick Mejía <ericknmejia@gmail.com>
2025-02-16 21:21:43 +0100

    guardar las categorias en una session

--- app/config/config.php
+++ app/config/config.php
@@ -2,3 +2,9 @@
 
 define('IMG_URL', '/public/assets/img/');
 define('ADMIN_URL', 'admin');
+
+if (! isset($_SESSION['categories'])) {
+    $categoryService = new App\Services\CategoryService;
+    $_SESSION['categories'] = $categoryService->show();
+}
+$categories = $_SESSION['categories'];
